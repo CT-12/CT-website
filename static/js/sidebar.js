857282247fc8e5d5b5d0fb929c7a3af34d3b3491
ben23515225566 <ben23515225566@gmail.com>
2025-02-13 12:45:42 +0800

    Adjusting sidebar display in article page

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -4,12 +4,4 @@ function toggleSidebar() {
     
     sidebar.classList.toggle("active");
     hamburger.classList.toggle("active"); // 讓漢堡變成 X
-
-    // 滑到回到頁面頂部
-    window.scrollTo(
-        {
-            top: 0,
-            behavior: "smooth" // 平滑滾動
-        }
-    )
 }
